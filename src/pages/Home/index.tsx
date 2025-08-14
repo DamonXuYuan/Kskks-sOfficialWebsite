bdc70850c10e27bce2587e14e117f425355b39d3
Damon <330718757@qq.com>
2025-08-14 17:31:23 +0800

    chagne: less

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
+
 import "./home.less";
 
 const Home: React.FC = () => {
-  return <div className="home">Init</div>;
+  return (
+    <section className="home">
+      <div className="test">Home</div>
+    </section>
+  );
 };
 
 export default Home;
