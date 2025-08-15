8906269fbac38006145f69ec3c2e066f372eb0e3
Damon <330718757@qq.com>
2025-08-15 09:43:01 +0800

    fix: name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/homePage";
 import Company from "./pages/company";
 import Layout from "./components/Layout";
 
