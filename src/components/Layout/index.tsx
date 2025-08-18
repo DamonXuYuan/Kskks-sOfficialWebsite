876ac72d385972477b8af7cb5e3203aff45fbb0f
Damon <330718757@qq.com>
2025-08-18 10:47:22 +0800

    add: particle

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./index.less";
 import Header from "../Header";
 import Footer from "../Footer";
+import ParticleAnimation from "../ParticleAnimation";
 
 interface LayoutProps {
   children?: React.ReactNode;
@@ -11,6 +12,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <section className="layout">
+      <ParticleAnimation />
       <Header />
       {children}
       <Footer />
